3c8bedd0fa27c11707d0bf87b675ec6d3feb44ca
Saman <samanezzatabadi@yahoo.com>
2026-02-05 10:37:49 +0330

    added check otp and verify token

--- digikala/module/auth/auth.routes.js
+++ digikala/module/auth/auth.routes.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
-const { sendOtpService, checkOtpService } = require("./auth.service");
+const { sendOtpService, checkOtpService, verifyRefreshToken } = require("./auth.service");
 
 const router = Router();
 
 router.post("/send-otp", sendOtpService);
 router.post("/check-otp", checkOtpService);
+router.post("/refresh-token", verifyRefreshToken);
 
 module.exports = {
   authRouter: router,
--- digikala/module/auth/auth.service.js
+++ digikala/module/auth/auth.service.js
@@ -1,6 +1,8 @@
 const createHttpError = require("http-errors");
 const { User, Otp } = require("../user/user.model");
-
+const jwt = require("jsonwebtoken");
+const { config } = require("dotenv");
+config();
 async function sendOtpService(req, res, next) {
   try {
     const { mobile } = req.body;
@@ -49,15 +51,61 @@ async function checkOtpService(req, res, next) {
     if (user?.otp?.code !== code) throw createHttpError(401, "otp code is invalid");
     if (user?.otp?.expires_in < new Date()) throw createHttpError(401, "otp code is expired");
 
+    const { access_token, refresh_token } = generateTokens({ userId: user.id });
+
     return res.json({
       message: "logged-in successfuly",
+      access_token,
+      refresh_token,
     });
   } catch (error) {
     next(error);
   }
 }
 
+async function verifyRefreshToken(req, res, next) {
+  try {
+    const { REFRESH_TOKEN_SECRET } = process.env;
+    const { token } = req.body;
+
+    if (!token) throw createHttpError(401, "refresh token required!");
+
+    const verifiedToken = jwt.verify(token, REFRESH_TOKEN_SECRET);
+    if (verifiedToken?.userId) {
+      const user = await User.findByPk(verifiedToken.userId);
+      if (!user) throw createHttpError(401, "login to your account!");
+
+      const { access_token, refresh_token } = generateTokens({ userId: user.id });
+
+      return res.json({
+        access_token,
+        refresh_token,
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
+function generateTokens(payload) {
+  const { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } = process.env;
+
+  const access_token = jwt.sign(payload, ACCESS_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
+  const refresh_token = jwt.sign(payload, REFRESH_TOKEN_SECRET, {
+    expiresIn: "30d",
+  });
+
+  return {
+    access_token,
+    refresh_token,
+  };
+}
+
 module.exports = {
   sendOtpService,
   checkOtpService,
+  generateTokens,
+  verifyRefreshToken
 };
